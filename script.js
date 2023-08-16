9cfe821c0069d5ca73afa12898a847b7e98976ef
Aditya kumar <Adityakumar14022002@gmail.com>
2023-08-16 17:16:42 +0530

    ques solve

---
+++ script.js
@@ -0,0 +1,72 @@
+
+// const obj = {
+//     firstName:"ADITYA",
+//     lastName:"Kumar",
+//     first:'Adi'
+// }
+
+// const map = new Map(Object.entries(obj));
+// console.log(obj)
+// const aa = map.has('fist')
+// console.log(aa)
+
+// // // map.set('firstName','Adi')
+// // // console.log(map)
+// // // map.set override kr rha data jo phele se data hai
+
+// // //map.get key vale se data deta hai
+// // // const name = map.get('first')
+// // // console.log(name)
+
+// // map.delete('first')
+// // map.delete('firstName')
+// // console.log(map)
+
+// // let arr = [[2],[6],[99]];
+
+// // arr.map((a)=>{
+// //     console.log(a)
+// // })
+
+
+
+// const newSet = new Set()
+// newSet.add(12)
+// newSet.add(18)
+// console.log(newSet)
+
+// .....................................................
+
+// const str = ' qwertyuiop asdfghjkl zxcvbnm '
+// const a = str.trim() 
+// const newSet = new Set();
+// for( let i = 0; i<a.length; i++){
+//  newSet.add(a.charAt(i))
+// }
+// console.log(newSet.size === 26)
+
+
+
+// const str = 'qwertyuiop asdfghjkl zxcvbnm'
+// const a = str.replace(" ","")
+// const newSet = new Set();
+// for( let i = 0; i < a.length; i++){
+//  newSet.add(a.charAt(i))
+// }
+// console.log(newSet.size === 26)
+
+
+//question solve
+
+// function calcSAreaOfCircle(radius){
+//     return Math.PI * radius**2;
+// }
+// console.log(Math.round(calcSAreaOfCircle(5)))
+
+//question solve 
+
+let arr = [12,34,45,65,43,32]
+
+function CalcMeanandMedian(arr){
+    let sum = 0;
+}
