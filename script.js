2285fa6ac2e50061aefcb659e8c0cc20973ace8f
Aditya kumar <Adityakumar14022002@gmail.com>
2023-08-22 16:48:52 +0530

    merge sort unsloved

---
+++ script.js
@@ -0,0 +1,27 @@
+// let arr = [12,54,65,32,67,21,8,3,23];
+// let chunk = arr.map(elem => [elem])
+// function mS(){return[...arr]}
+// function mergeSort(arr){
+//    let newArr = [];
+//    let first = 0 , second=0;
+//    while (first <= arr.length && second <= arr.length){
+//       const mergeArr = mS(arr[first],arr[second])
+//       newArr.push(arr[first])
+
+//       return newArr
+//    }
+//    first+=2;
+//    second+=2;
+// }
+// mergeSort(arr)
+
+
+
+
+const arr = [1,2,3,4,5,6,7,8,9,23,11];
+
+
+// for(let i = 0; i<arr.length; i+=2){
+//     let j = i+1
+//     console.log({i:arr[i],j:arr[j]});
+// }
