c854b4d8f1d51b8352bd1f265a3d6b5a0b18cf4c
Aditya kumar <Adityakumar14022002@gmail.com>
2023-08-23 14:17:55 +0530

    form correction

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let firstname = document.getElementById("firstname");
-let lastname = document.getElementById("lastname");
-let age = document.getElementById("age");
-let phone = document.getElementById("phone");
-let mail = document.getElementById("mail")
-let male = document.getElementById("male");
-let female = document.getElementById("female");
-let error = document.getElementById("error")
-let sumit = document.getElementById("submit")
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const age = document.getElementById("age");
+const phone = document.getElementById("phone");
+const mail = document.getElementById("mail")
+const male = document.getElementById("male");
+const female = document.getElementById("female");
+const error = document.getElementById("error")
+const submit = document.getElementById("submitButton")
 
-sumit.addEventListener("click" , () =>{
-    sumit.innerText="sumited"
+submit.addEventListener("click" , () =>{
+    submit.innerText="sumited"
 })
 
 function validationInput(input){
     if(input ==='phone' && phone.value.length !== 10){
         error.innerText='bhai plz 10 number do'
-    }else if(input === 'age' && age.value < 18){
+    }
+    else{
+        error.innerText=""
+    }
+}
+function Agevalidate(input){
+    if(input === 'age' && age.value < 18){
         error.innerText="you are not old enough"
     }else{
         error.innerText=""
@@ -23,7 +29,6 @@ function validationInput(input){
 }
 function handleForm(event){
     event.preventDefault()
-    q
     console.log({
         firstname:firstname.value,
         lastname: lastname.value,
